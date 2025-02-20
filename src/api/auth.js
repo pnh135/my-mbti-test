4ab8694cd84e01bcc8d5c77c5aa3cad501c661c7
nahyun park <pnh13584@gmail.com>
2025-02-20 15:44:26 +0900

    Update: api url 변경

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "";
+const API_URL = "https://www.nbcamp-react-auth.link";
 
 export const register = async (userData) => {
   const response = await axios.post(`${API_URL}/register`, userData);
